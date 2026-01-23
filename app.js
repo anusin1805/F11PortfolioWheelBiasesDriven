c05d23faedbd2fc7b44d6238c5d6e095ba4e302d
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-01-23 16:17:21 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// Import your new component
+import FinWiseIntegration from './FinWiseIntegration';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Render the dashboard here */}
+      <FinWiseIntegration />
+    </div>
+  );
+}
+
+export default App;
